5e7eb705d2f00cd69634e51e6e10512cf4fe1404
Christian Vetter <cvetter34@gmail.com>
2014-04-07 12:19:42 +0800

    Imported new User model with ActiveRecord.

---
+++ app/models/user.rb
@@ -0,0 +1,67 @@
+require 'bcrypt'
+
+PASSWORD_RESET_EXPIRES = 4
+
+class User < ActiveRecord::Base
+
+  attr_accessor :password, :password_confirmation
+
+  before_save :set_random_password, :encrypt_password
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, confirmation: true
+
+  def self.authenticate email, password
+    user = User.find_by email: email
+    user if user and user.authenticate(password)
+  end
+
+  def self.find_by_code code
+    if user = User.find_by({:code => code, :expires_at => {"$gte" => Time.now.gmtime}})
+      user.set_expiration
+    end
+    user
+  end
+
+  def authenticate password
+    self.fish == BCrypt::Engine.hash_secret(password, self.salt)
+  end
+
+
+  def set_password_reset
+    self.code = SecureRandom.urlsafe_base64
+    set_expiration
+  end
+
+  def set_expiration
+    self.expires_at = PASSWORD_RESET_EXPIRES.hours.from_now
+    self.save
+  end
+
+  def reset_password(params)
+    if params[:password].blank?
+      self.errors.add(:password, "can't be blank")
+      false
+    else
+      if self.update_attributes(params)
+        self.update_attributes(params.merge( code: nil, expires_at: nil ))
+      end
+    end
+  end
+
+  protected
+
+    def set_random_password
+      if self.fish.blank? and password.blank?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(SecureRandom.base64(32), self.salt)
+      end
+    end
+
+    def encrypt_password
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+      end
+    end
+
+end
---
+++ db/migrate/20140407041043_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :password_confirmation
+      t.string :salt
+      t.string :fish
+
+      t.timestamps
+    end
+  end
+end
