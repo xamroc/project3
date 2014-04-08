8b8cb1a4c0fa4cc92be0e8555a945069d4bbc4e9
JulesWiz <ngjulie0203@gmail.com>
2014-04-08 17:35:51 +0800

    Fixed testing for UserController

--- spec/controllers/user_controllers_spec.rb
+++ spec/controllers/user_controllers_spec.rb
@@ -4,12 +4,14 @@ describe UsersController do
 
   render_views
 
-  before :each do
+  before :all do
     User.create(email: 'cvetter34@gmail.com', password: '1234', password_confirmation: '1234')
+    User.create(email: 'marcolau@gmail.com', password: '1234', password_confirmation: '1234')
+    User.create(email: 'julie@gmail.com', password: '1234', password_confirmation: '1234')
   end
 
   describe 'GET index' do
-    it "return list of users" do
+    it "return index of users" do
       get :index, :format => :json
       expect(response.status).to eq 200
       expect(response.body).to include 'cvetter34@gmail.com'
@@ -18,10 +20,11 @@ describe UsersController do
   end
 
   describe 'GET show' do
-    it "returns one user" do
-      get :show, :format => :json
-      expect(response.status).to eq 1
-      expect(JSON.load(response.body)["users"][0]["email"]).to eq 'cvetter34@gmail.com'
+    it "returns index of users" do
+      get :show, :id =>, :format => :json
+      expect(response.status).to eq 200
+      expect(response.body).to_not be_nil
+      expect(JSON.load(response.body)["email"]).to eq 'cvetter34@gmail.com'
     end
   end
 
