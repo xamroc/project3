89975077289af995578526d59b17e1c4bb09eb1a
Marco Lau <lau.career@gmail.com>
2014-04-09 16:32:37 +0800

    Fixed by spec files renaming misspelled user_controller_spec.rb to users_controller_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Project3::Application.routes.draw do
   root 'site#index'
 
   resources :site
-  resources :tools
 
   get  'register' => 'registration#new', as: :register
   post 'register' => 'registration#create'
