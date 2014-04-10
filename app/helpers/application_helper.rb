db4efcabf29a074936abe7e8ab36e1c944182b68
Christian Vetter <cvetter34@gmail.com>
2014-04-10 11:59:26 +0800

    Added noty and login/logout buttons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,53 @@
 module ApplicationHelper
+
+  def flash_noty_script_tag
+    javascript_tag(
+      %{
+        $(function() {
+          #{ flash_to_noties.compact.join(" ") }
+        });
+      }.squish
+    ) unless flash.empty?
+  end
+
+  private
+
+  def get_type(name)
+    name == :notice ? "success" : "error"
+  end
+
+  def get_timeout(name)
+    name == :notice ? 4000 : 30000
+  end
+
+  def errors_to_ul(errors)
+    content_tag(:ul,
+      errors.to_a.map do |message|
+        content_tag( :li, message ) unless message.blank?
+      end.compact.join.html_safe
+    ) unless errors.empty?
+  end
+
+  def get_text(message)
+    message.is_a?(ActiveModel::Errors) ? errors_to_ul(message) : message
+  end
+
+  def flash_to_noties
+    flash.map do |name, message|
+      if message.is_a?(String) || message.is_a?(ActiveModel::Errors)
+        get_noty_call get_type(name), get_timeout(name), get_text(message)
+      end
+    end
+  end
+
+  def get_noty_call(type, timeout, text)
+    %{
+      noty({
+        layout: "top",
+        type: "#{type}",
+        timeout: "#{timeout.to_s}",
+        text: "#{text}"
+      });
+    }.squish
+  end
 end
