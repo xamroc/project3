2f74b972a57b7a6c95571545d0bf09ba5912898d
JulesWiz <ngjulie0203@gmail.com>
2014-04-11 17:32:25 +0800

    Added create user and tool to TransactionController

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -6,7 +6,20 @@ describe TransactionsController do
   render_views
 
   before :all do
-    Transaction.create(user_id: '2', tool_id:'2')
+    user = User.new(email: 'julie123@gmail.com', password: '1234', password_confirmation: '1234')
+    user.save
+    tool = Tool.new(name: 'Chainsaw', category: 'Tools & Garden', description: 'Put on a hockey mask.')
+    tool.save
+    Transaction.create(user_id: '1', tool_id: '1')
+  end
+
+  describe "GET index" do
+    it "returns list of transactions" do
+      get :index, :format => :json
+      expect(response.status).to eq 200
+      expect(response.body).to include '1'
+      expect(JSON.load(response.body)["transactions"][0]["user_id"]).to eq 1
+    end
   end
 
   describe "GET show" do
@@ -15,16 +28,7 @@ describe TransactionsController do
       get :show, :id => 1, :format => :json
       expect(response.status).to eq 200
       expect(response.body).to_not be_nil
-      expect(JSON.load(response.body)["user_id"]).to eq 2
-    end
-  end
-
-  describe "GET index" do
-    it "returns list of transactions" do
-      get :index, :format => :json
-      expect(response.status).to eq 200
-      expect(response.body).to include '2'
-      expect(JSON.load(response.body)["transactions"][0]["user_id"]).to eq 2
+      expect(JSON.load(response.body)["user_id"]).to eq 1
     end
   end
 
